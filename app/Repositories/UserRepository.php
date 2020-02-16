3285c1b1d449137feaccc43c461985621dedebeb
omarfawzi <omarfawzi96@gmail.com>
2020-02-16 15:19:21 +0200

    remove unused classes

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,13 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Events\UserCreated;
-use App\Events\UserUpdated;
 use App\Models\User;
-use App\Transformers\UserTransformer;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 
 class UserRepository
 {
