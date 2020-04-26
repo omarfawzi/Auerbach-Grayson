11838c78efd2bb3b8540659f1931783ea99fcfb6
omarfawzi <omarfawzi96@gmail.com>
2020-04-26 19:01:42 +0200

    rename vars

--- app/Services/WeightAssignationService.php
+++ app/Services/WeightAssignationService.php
@@ -29,41 +29,45 @@ class WeightAssignationService
     public function assign()
     {
         $userId = Auth::getAuthenticatedUser()->id;
-
         $client = Auth::getAuthenticatedUser()->getClient();
 
         $lastAssignedDate = Carbon::now()->subMonths(6)->toDateTime();
 
-        $reportWeight     = ReportWeight::select('updated_at')->where('user_id', $userId)->orderBy('updated_at', 'desc')->first();
-
+        $reportWeight = ReportWeight::select('updated_at')->where('user_id', $userId)->orderBy('updated_at', 'desc')->first();
         if ($reportWeight instanceof ReportWeight) {
             $lastAssignedDate = new DateTime($reportWeight->updated_at);
         }
 
-        $companyEventEntitiesIds = Arr::pluck($this->iplannerService->getEventEntities(
-            $lastAssignedDate,
-            [EventCodes::CONFERENCE_CODE, EventCodes::MEETING_CODE],
-            $client->IPREO_ContactID
-        )['DmEventEntity'],'company_id');
+        $companyEventEntitiesIds = Arr::pluck(
+            $this->iplannerService->getEventEntities(
+                $lastAssignedDate,
+                [EventCodes::CONFERENCE_CODE, EventCodes::MEETING_CODE],
+                $client->IPREO_ContactID
+            )['DmEventEntity'],
+            'company_id'
+        );
 
         $companySubscriptionsIds = Subscription::select('subscribable_id')->where(
             ['subscribable_type' => Subscription::COMPANY_SUBSCRIPTION_TYPE, 'user_id' => $userId]
         )->get()->pluck('subscribable_id')->toArray();
 
-        $this->assignWeights($userId,$companyEventEntitiesIds,2);
-        $this->assignWeights($userId,$companySubscriptionsIds,1);
+        $this->assignWeights($userId, $companyEventEntitiesIds, 2);
+        $this->assignWeights($userId, $companySubscriptionsIds, 1);
     }
 
-    private function assignWeights(int $userId , array $companyIds , int $weight)
+    private function assignWeights(int $userId, array $companyIds, int $weight) : void
     {
         $now = Carbon::now('utc')->toDateTimeString();
-        $existingRecords = ReportWeight::whereIn('company_id',$companyIds)->where('user_id',$userId)->pluck('company_id')->toArray();
-        $newRecords = array_diff($companyIds,$existingRecords);
+        $existingCompanyIds = ReportWeight::whereIn('company_id', $companyIds)->where('user_id', $userId)->pluck(
+            'company_id'
+        )->toArray();
+
+        $newCompanyIds = array_diff($companyIds, $existingCompanyIds);
 
         $bulkInsertData = [];
-        foreach ($newRecords as $newRecord) {
+        foreach ($newCompanyIds as $newCompanyId) {
             $bulkInsertData[] = [
-                'company_id' => $newRecord,
+                'company_id' => $newCompanyId,
                 'user_id'    => $userId,
                 'weight'     => $weight,
                 'created_at' => $now,
@@ -73,7 +77,6 @@ class WeightAssignationService
 
         ReportWeight::insert($bulkInsertData);
         ReportWeight::increment('weight', $weight, ['updated_at' => Carbon::now()]);
-
     }
 
 }
